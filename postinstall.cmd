5856bbb7e2e009ab63ac79c13e0b79061dc6dbf0
Maksym Romanov <maksym.romanov@brighteye.com.ua>
2023-08-04 12:56:46 +0300

    system npm

--- postinstall.cmd
+++ postinstall.cmd
@@ -1,11 +1,1 @@
-(if not exist \home mkdir \home)
-(if not exist \home\nodered mkdir \home\nodered)
-(if not exist \home\nodered\.node-red mkdir \home\nodered\.node-red)
-copy npm_.cmd \home\nodered\.node-red\npm_.cmd
-pushd \home\nodered\.node-red
-
-npm install node-red & (
-     copy npm_.cmd npm.cmd /y
-     popd 
-     copy npm_.cmd npm.cmd /y
-)
+npm install node-red-dashboard node-red-contrib-azureiothubnode node-red-contrib-cognitive-services node-red-contrib-azure-blob-storage node-red-contrib-azure-table-storage node-red-contrib-azure-sql node-red-contrib-azure-documentdb node-red-contrib-azureiotc2d node-red-node-swagger
